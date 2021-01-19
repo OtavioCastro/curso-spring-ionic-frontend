1ddea261f7c4c40b2f9e80bfb1e3c1fded0af0f7
Otávio de Castro <otavio.vn01@hotmail.com>
2021-01-19 14:05:21 -0300

    Terminando as funcionalidades do carrinho

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ProdutoService } from '../../services/domain/produto.service';
 
 /**
@@ -18,30 +19,36 @@ import { ProdutoService } from '../../services/domain/produto.service';
 })
 export class ProdutoDetailPage {
 
-item: ProdutoDTO;
+  item: ProdutoDTO;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
     let produto_id = this.navParams.get('produto_id');
     this.produtoService.findById(produto_id)
-    .subscribe(response => {
-      this.item = response;
-      this.getImageUrlIfExists();
-    },
-    error => {});
+      .subscribe(response => {
+        this.item = response;
+        this.getImageUrlIfExists();
+      },
+        error => { });
   }
 
-  getImageUrlIfExists(){
+  getImageUrlIfExists() {
     this.produtoService.getImageFromBucket(this.item.id)
-    .subscribe(response => {
-      this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
-    },
-    error => {});
+      .subscribe(response => {
+        this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+      },
+        error => { });
+  }
+
+  addToCart(produto: ProdutoDTO) {
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
