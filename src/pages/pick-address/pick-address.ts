61f8d3bd70c2857bcf25df2cc742b29ba596cf5e
Otávio de Castro <otavio.vn01@hotmail.com>
2021-01-20 16:20:09 -0300

    Armazenamento dos dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
 
@@ -12,29 +14,52 @@ import { StorageService } from '../../services/storage.service';
 export class PickAddressPage {
 
   items: EnderecoDTO[];
-  
+
+  pedido: PedidoDTO;
+
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public clienteService: ClienteService,
-    public StorageService: StorageService) {
+    public storageService: StorageService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
-    let localUser = this.StorageService.getLocalUser();
+    let localUser = this.storageService.getLocalUser();
     if (localUser && localUser.email) {
-    this.clienteService.findByEmail(localUser.email)
-    .subscribe(response =>{
-      this.items = response['enderecos'];
-    },
-    error => {
-      if (error.status == 403) {
-        this.navCtrl.setRoot('HomePage');
-      }
-    });  
-  } else {
-    this.navCtrl.setRoot('HomePage');
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: { id: response['id'] },
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.items.map(
+              x => {
+                return {
+                  quantidade: x.quantidade,
+                  produto: { id: x.produto.id }
+                }
+              })
+          }
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    } else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
-}
+
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
+  }
 
 }
