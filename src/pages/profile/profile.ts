62c5811312dd666849103ef41700ca2b0e587ccc
Otávio de Castro <otavio.vn01@hotmail.com>
2021-01-26 14:39:17 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -33,6 +33,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -75,6 +79,19 @@ export class ProfilePage {
     }, (err) => {
      
     });
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null;
+      this.loadData();
+    },
+    error => {});
+  }
+
+  cancel(){
+    this.picture = null;
   }
 
 }
